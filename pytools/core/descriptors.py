26c9844a110ba4d8d21b4e76d47d47a99e1fdbce
Chad Dotson <chad@cdotson.com>
2016-12-04 23:39:12 -0600

    Convert print to logging and add DateTimeAttribute.

--- pytools/core/descriptors.py
+++ pytools/core/descriptors.py
@@ -1,9 +1,16 @@
+from datetime import datetime
+from logging import getLogger
+
+logger = getLogger(__name__)
+
 
 class Attribute(object):
     def __init__(self, name="", types=None, default=None, *args, **kwargs):
         self._valid_types = types if type(types) is list or types is None else [types]
 
-        print("name", name,"-", default, "-",types, "-", args, "-",kwargs)
+        logger.debug("%s(name=%s, types=%s, default=%s, args=%s, kwargs=%s)",
+            self.__class__.__name__, name, types, default, args, kwargs)
+
         self._name = "__" + name
 
         if default is not None:
@@ -47,7 +54,6 @@ class NumberAttribute(Attribute):
         super(NumberAttribute, self).__init__(*args, types=[int, float], **kwargs)
 
 
-
 class StringAttribute(Attribute):
     def __init__(self, max_length=None, *args, **kwargs):
         self._max_length = max_length
@@ -58,3 +64,21 @@ class StringAttribute(Attribute):
 
         if self._max_length is not None and len(val) > self._max_length:
             raise TypeError("length must be <={0}".format(self._max_length))
+
+
+class DateTimeAttribute(Attribute):
+    def __init__(self, start_date=None, end_date=None, *args, **kwargs):
+        self._start_date = start_date
+        self._end_date = end_date
+
+        super(DateTimeAttribute, self).__init__(*args, types=datetime, **kwargs)
+
+    def _assert_valid(self, val):
+        super(DateTimeAttribute, self)._assert_valid(val)
+
+        if self._start_date is not None and val < self._start_date:
+            raise TypeError("date must be on or after {0}".format(self._start_date))
+
+        if self._end_date is not None and val > self._end_date:
+            raise TypeError("date must be on or before {0}".format(self._end_date))
+
